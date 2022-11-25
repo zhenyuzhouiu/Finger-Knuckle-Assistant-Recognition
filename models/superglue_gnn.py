38320661c5320138a9ad32957c6402698111fda4
zhenyuzhouiu <zhenyuzhouiu@gmail.com>
2022-11-25 19:16:52 +0800

    update superglue_gnn parameter weights to weight

--- models/superglue_gnn.py
+++ models/superglue_gnn.py
@@ -226,7 +226,7 @@ class SuperGlue(nn.Module):
             self.config['descriptor_dim'], self.config['descriptor_dim'],
             kernel_size=1, bias=True)
 
-        if self.config['weight'] == '':
+        if self.config['weight'] != '':
             path = self.config['weight']
             self.load_state_dict(torch.load(str(path)))
             print('Loaded SuperGlue model (\"{}\" weights)'.format(
